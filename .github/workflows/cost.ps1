9f03d1b6a34638564cb2de6969a4773dd96579ff
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2025-03-11 14:54:58 +0100

    remove files from git

--- .github/workflows/cost.ps1
+++
@@ -1,5 +0,0 @@
-$kicostPath = "C:\Users\pasca\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\kicost"
-$configPath = "kicost.yaml"
-
-# Run kicost
-& $kicostPath -c .github/workflows/kicost.yaml -i outputs/bom/kicost-bom.csv --currency EUR
